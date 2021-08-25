93d45db25a9bf58e09f6dd16a4cd62bf4997a508
rainverted <MY_NAME@example.com>
2021-08-25 18:36:58 +0300

    Server-side cookies

--- public/js/login.js
+++ public/js/login.js
@@ -48,8 +48,11 @@ function submitFormInfo(e) {
 function responseAction(response) {
     try {
         const responseObject = JSON.parse(response);
+        if (responseObject.error) {
+            localStorage.removeItem('login-token');
+            showMessage('error', responseObject.error);
+        }
         localStorage.setItem('login-token', responseObject.token);
-        document.cookie = 'login-token=' + responseObject.token;
         showMessage('success', 'Token gautas ;)');
     } catch (error) {
         showMessage('error', 'Serverio klaida!');
