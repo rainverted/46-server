c00ddd1f7dc61cf388987fbbecf206af45e6b64a
rainverted <MY_NAME@example.com>
2021-09-01 18:05:20 +0300

    MySQL

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+const mysql = require('mysql2/promise');
 const server = require('./lib/server');
 
 
 const app = {}
 
 
-app.init = () => {
+app.init = async () => {
     //paruosti reikiamus direktorijas
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'barsukas'
+    });
 
     //paruosti reikiamus failus
 
     //inicijuojame serveri
-    server.init();
+    server.init(connection);
 }
 
 app.init();
