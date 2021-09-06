e994cfc1f1d4ffab5d3e003abececcf13d9d822e
rainverted <MY_NAME@example.com>
2021-09-06 16:47:41 +0300

    MySQL

--- index.js
+++ index.js
@@ -14,7 +14,50 @@ app.init = async () => {
     });
 
     //paruosti reikiamus failus
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'barsukas',
+    });
 
+    let sql = '';
+    let rows = [];
+
+    // perskaitom ka turim is pradziu
+    sql = 'SELECT * \
+    FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // irasom nauja masina
+    sql = 'INSERT INTO `cars` \
+        (`id`, `marke`, `modelis`, `color`, `engine`, `doors`) \
+        VALUES (5, "Audi", "100", "grey", "2.2", "3")';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // atnaujiname
+    sql = 'UPDATE `cars` \
+        SET `marke` = "Opel", \
+            `modelis` = "Zafyra", \
+            `color` = "grey" \
+        WHERE `cars`.`id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // perskaitom ka turim po atnaujinimo
+    sql = 'SELECT * \
+    FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+
+    // istrinam
+    sql = 'DELETE FROM `cars` \
+    WHERE `id` = 5';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
     //inicijuojame serveri
     server.init(connection);
 }
